f5fcab7b640a32af289f8136f1b50342cae95ae8
oskarr <oskarrune@gmail.com>
2021-02-26 20:51:03 +0100

    Added adder buttons

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var app = new Vue({
         inputty: "",
         autorender: true,
         
-        showCheatSheet: true,
+        showCheatSheet: false,
 
+        cTextColor: "#ffffff",
         cBkgColor: "#333333",
-        cTextColor: "#cccccc",
         cBorder: "",
 
         themes: {
@@ -45,7 +45,7 @@ var app = new Vue({
     },
     methods: {
         inputCallback: function(e) {
-            if (e.which == 13 || this.autorender)
+            if (this.autorender || e.which == 13)
                 this.render()
         },
         render: function(force = false) {
@@ -58,6 +58,43 @@ var app = new Vue({
             if("border" in this.themes[this.theme]) {
                 this.cBorder = this.themes[this.theme].border;
             } else {this.cBorder = "";}
-        }
+        },
+        insertLaTeX: function(text) {
+            // will give the current postion of the cursor
+            el = document.getElementById("inputty")
+            curPos = el.selectionStart; 
+
+            // will get the value of the text area
+            x = el.value
+
+            // setting the updated value in the text area
+            this.inputty = x.slice(0,curPos)+text+x.slice(curPos);
+            this.inputCallback({which: 0})
+        },/*
+        getSVG: function() {
+            data = document.getElementById("putty").childNodes[1].firstChild.innerHTML
+            head = '<?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" version="1.2"'
+            console.log(data)
+            data = head + data.substring(4).replace(/currentColor/g, "#000")
+            return data
+        },
+        downloadSVG: function() {
+            download(this.getSVG(), "MathJax.svg", "image/svg+xml")
+        }, */
     },
 })
+
+/*
+clipboard = async function (event) {
+    console.log("EVENT")
+    if (!navigator.clipboard) {
+        alert("Your browser is too old.")
+        return
+    }
+    try {
+        await navigator.clipboard.writeText([app.getSVG()])
+    } catch (err) {
+        console.error('Failed to copy!', err)
+    }
+}
+*/
