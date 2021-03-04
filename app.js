2a62208b1461c59011c025fda5ee14ae826f11b7
oskarr <oskarrune@gmail.com>
2021-03-04 21:02:50 +0100

    More insertions

--- app.js
+++ app.js
@@ -59,17 +59,27 @@ var app = new Vue({
                 this.cBorder = this.themes[this.theme].border;
             } else {this.cBorder = "";}
         },
-        insertLaTeX: function(text) {
+        insertLaTeX: function(text, offset) {
+            var offset = (typeof offset !== 'undefined') ?  offset : 0
             // will give the current postion of the cursor
             el = document.getElementById("inputty")
-            curPos = el.selectionStart; 
+            // TODO, make things with nonzero offset insert things on both sides of the selected text.
+            var selStart = el.selectionStart + 0;
+            var selEnd = el.selectionEnd + 0;
 
             // will get the value of the text area
-            x = el.value
+            var x = el.value
 
             // setting the updated value in the text area
-            this.inputty = x.slice(0,curPos)+text+x.slice(curPos);
+            el.value = x.slice(0,selEnd)+text+x.slice(selEnd);
+
+            // Force re-render.
+            this.inputty = el.value
             this.inputCallback({which: 0})
+            
+            el.focus()
+            var tarPos = selEnd + offset + text.length
+            el.setSelectionRange(tarPos, tarPos)
         },/*
         getSVG: function() {
             data = document.getElementById("putty").childNodes[1].firstChild.innerHTML
